d41aecd36a6d030274eaa20ebc9a90f067a9050b
Arnel Forbes <forbesarnel09@gmail.com>
2021-10-27 15:17:36 +0800

    Separate view and logic

--- index.php
+++ index.php
@@ -1,28 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Php Lessons</title>
-    <style>
-        header {
-            background-color: #e3e3e3;
-            padding: 2em;
-            text-align: center;
-        }
-    </style>
-</head>
+$name = htmlspecialchars(ucfirst(strtolower($_GET['name'])));
+$greetings = "Hello, {$name}";
 
-<body>
-    <header>
-        <h1>
-            <!-- Test push -->
-            <!-- test change to try commit signing -->
-            <?= 'Hello, ' . htmlspecialchars(ucfirst(strtolower($_GET['name']))); ?>
-        </h1>
-    </header>
-</body>
-
-</html>
+require 'index.view.php';
