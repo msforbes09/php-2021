53ff2a0116aa1e0385c143d028e00f7866928806
Arnel Forbes <forbesarnel09@gmail.com>
2021-11-03 19:01:32 +0800

    PDO introduction

--- functions.php
+++ functions.php
@@ -14,12 +14,30 @@ function dd($var)
 }
 
 /**
- * Check age
+ * Connect to DB
  *
- * @param integer $age
- * @return boolean
+ * @return PDO
  */
-function checkAge(int $age) : bool
+function connectToDB()
 {
-    return $age >= 18;
+    try {
+        return new PDO('mysql:host=127.0.0.1;dbname=mytodo', 'root', 'password');
+    } catch (PDOException $e) {
+        die($e->getMessage());
+    }
+}
+
+/**
+ * Fetch all tasks
+ *
+ * @param PDO $pdo
+ * @return array
+ */
+function fetchAllTasks(PDO $pdo)
+{
+    $statement = $pdo->prepare('SELECT * FROM todos');
+
+    $statement->execute();
+
+    return $statement->fetchAll(PDO::FETCH_CLASS, 'Task');
 }
--- index.php
+++ index.php
@@ -1,51 +1,10 @@
 <?php
 
 require 'functions.php';
-class Task
-{
-    public $description;
+require 'Task.php';
 
-    public $completed = false;
+$pdo = connectToDB();
 
-    /**
-     * Class constructor
-     *
-     * @param [type] $description
-     */
-    public function __construct($description)
-    {
-        $this->description = $description;
-    }
-
-    /**
-     * Is task completed
-     *
-     * @return boolean
-     */
-    public function isCompleted()
-    {
-        return $this->completed;
-    }
-
-    /**
-     * Complete task
-     *
-     * @return void
-     */
-    public function complete()
-    {
-        $this->completed = true;
-    }
-}
-
-$tasks = [
-    new Task('Go to store'),
-    new Task('Learn PHP'),
-    new Task('Finish tickets'),
-];
-
-$tasks[1]->complete();
-
-// dd($tasks);
+$tasks = fetchAllTasks($pdo);
 
 require 'index.view.php';
